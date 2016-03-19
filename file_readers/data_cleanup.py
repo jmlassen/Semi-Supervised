bd21ff706ba6b3fc9808f182c649ceacd6f60cce
Joel Lassen <jmlassen@gmail.com>
2016-03-18 22:05:31 -0600

    Added ability to check language.

--- file_readers/data_cleanup.py
+++ file_readers/data_cleanup.py
@@ -1,6 +1,10 @@
 import csv
 import os
+import string
 import webbrowser
+from random import shuffle
+
+from nltk.corpus import stopwords
 
 users_file = '../dataset/users.csv'
 tweets_dir = '../dataset/tweets/'
@@ -15,38 +19,74 @@ def add_user_file(file, label):
 
 def print_gender_counts(records):
     genders = {}
+    total = 0
     for record in records:
         if record[1] not in genders:
             genders[record[1]] = 1
         else:
             genders[record[1]] += 1
-    print(genders)
+        if record[1] != "unlabeled" and not record[1].startswith("remove"):
+            total += 1
+    print("Total: {} -> {}".format(total, genders))
 
 
-def main_new():
+def possibly_non_english(filename):
+    try:
+        tweets_file = open(filename)
+        tweets = _read_file(tweets_file)
+    except UnicodeDecodeError:
+        tweets_file = open(filename, encoding='utf8')
+        tweets = _read_file(tweets_file)
+    words = tweets.split(' ')
+    stop_count = 0
+    stop = stopwords.words('english')
+    for word in words:
+        if word in stop:
+            stop_count += 1
+    print("Stop count: {}".format(stop_count))
+    if stop_count < 200:
+        return True
+    else:
+        return False
+
+
+def _read_file(file):
+    tweets = ""
+    file_reader = csv.reader(file)
+    for row in file_reader:
+        if len(row) > 0 and row[0].startswith("RT "):
+            exclude = set(string.punctuation)
+            row[0] = (''.join(ch for ch in row[0] if ch not in exclude)).lower()
+            tweets += row[0] + ' '
+    return tweets
+
+
+def data_cleanup_ui():
     records = []
     # Get all the records in the file
     with open(new_users) as f:
         f_reader = csv.reader(f)
         for row in f_reader:
             records.append([row[0], row[1]])
+    shuffle(records)
     for i in range(len(records)):
-        if records[i][1] != 'remove' and records[i][1] != 'unlabeled-l':
+        url = "https://twitter.com/{}".format(records[i][0])
+        filename = tweets_dir + records[i][0] + ".csv"
+        if records[i][1] == 'unlabeled':
             print_gender_counts(records)
-            url = "https://twitter.com/{}".format(records[i][0])
             webbrowser.open_new_tab(url)
-            gender = input("[1] M, [2] F, [3] U, [4] R: ")
-            if gender == '4':
-                print("Removing {}.csv".format(records[i][0]))
-                records[i][1] = 'remove'
-            elif gender == '1':
+            prompt_input = input("[1] M, [2] F, [3] R: ")
+            if prompt_input == '1':
                 records[i][1] = 'male'
-            elif gender == '2':
+            elif prompt_input == '2':
                 records[i][1] = 'female'
-            elif gender == '3':
-                records[i][1] = 'unlabeled-l'
-            else:
+            elif prompt_input == '3':
+                why = input("Why: ")
+                records[i][1] = 'remove-' + why
+            elif prompt_input == 'q':
                 break
+            else:
+                records[i][1] = "unlabeled-" + prompt_input
     os.remove(new_users)
     for record in records:
         add_user_file(record[0], record[1])
@@ -72,4 +112,4 @@ def main():
 
 if __name__ == "__main__":
     # main()
-    main_new()
+    data_cleanup_ui()
