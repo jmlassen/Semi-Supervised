199ace4c82b91b4a2b37fbf83596df1b232aa82e
Joel Lassen <jmlassen@gmail.com>
2016-03-28 23:12:08 -0600

    Changed weight, modified research code to run benchmark classifier with semi supervised classifier.

--- research/research.py
+++ research/research.py
@@ -1,3 +1,4 @@
+from sklearn.metrics import accuracy_score
 from sklearn.utils import shuffle
 
 from classifiers.semi_supervised_knn_classifier import SemiKnnClassifier
@@ -10,18 +11,19 @@ def run_benchmark_test(tweets):
     return results
 
 
-def run_benchmark_graph_point_generation(tweets, clf, run_with_unlabeled=False):
+def run_benchmark_graph_point_generation(tweets, run_with_unlabeled=False):
     # Set file names where we are going to save our results
     results_file = 'semi-nb-results-low-unlabeled-weight.csv'
     confusion_matrix_file = 'semi-nb-confusion-matrix-low-unlabeled-weight.csv'
     data, target = shuffle(tweets.data, tweets.target)
+    clf = SemiNbClassifier()
+    clf_not_semi = SemiNbClassifier()
     for i in range(10, len(data[100:])):
         confusion_matrix = {}
-        if run_with_unlabeled:
-            clf.fit(data[100:(100 + i)], target[100:(100 + i)], tweets.unlabeled)
-        else:
-            clf.fit(data[100:(100 + i)], target[100:(100 + i)])
+        clf.fit(data[100:(100 + i)], target[100:(100 + i)], tweets.unlabeled)
+        clf_not_semi.fit(data[100:(100 + i)], target[100:(100 + i)])
         predicted_values = clf.predict(data[:100])
+        predicted_values_not_semi = clf_not_semi.predict(data[:100])
         correct = 0
         for j, prediction_point in enumerate(predicted_values):
             if prediction_point == target[j]:
@@ -34,6 +36,8 @@ def run_benchmark_graph_point_generation(tweets, clf, run_with_unlabeled=False):
             else:
                 confusion_matrix[target[j]][prediction_point] += 1
         accuracy = round(correct / len(data[:100]), 5)
+        accuracy_not_semi = accuracy_score(target[:100], predicted_values_not_semi)
+        print("(No Labeled Data) {} -> {}".format(i, accuracy_not_semi))
         print("{} -> {}".format(i, accuracy))
         with open(results_file, "a") as file:
             file.write("{},{}\n".format(i, accuracy))
@@ -49,7 +53,7 @@ def run_benchmark_graph_point_generation(tweets, clf, run_with_unlabeled=False):
 
 def main():
     tweet_train = MultiFileReader().read_labeled_an_unlabeled_data('../dataset/users_new.csv', '../dataset/tweets/', 2)
-    run_benchmark_graph_point_generation(tweet_train, SemiNbClassifier(), run_with_unlabeled=True)
+    run_benchmark_graph_point_generation(tweet_train, run_with_unlabeled=True)
 
 if __name__ == '__main__':
     main()
